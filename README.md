b20d723eb39c884d15349ffc8c057841d4cda4df
unknown <2462177270@qq.com>
2021-02-22 16:07:57 +0800

    类关系

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ sematic ui
 
 # 后端
 SpringBoot；javaweb+Thymeleaf+JPA+mysql+DevTools
+
+#数据库
+jdbc+mysql
 				
 # 管理
 maven
---
+++ src/main/java/com/blog/demo/blogcore/User.java
@@ -0,0 +1,127 @@
+package com.blog.demo.blogcore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "t_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nickName;
+    private String userName;
+    private String passWord;
+    private String email;
+    private String avatar;
+    private Integer type;
+    //创建时间
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+    //更新时间
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+    @OneToMany(mappedBy = "use")
+    private List<Blog> blogs = new ArrayList<>();
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public List<Blog> getBlogs() {
+        return blogs;
+    }
+
+    public void setBlogs(List<Blog> blogs) {
+        this.blogs = blogs;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Use{" +
+                "id=" + id +
+                ", nickName='" + nickName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                ", email='" + email + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", type=" + type +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
+}
